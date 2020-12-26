0cad9957a8e749f19dcce6939c33598ae2b867b1
Daniel Usani <daniel.usani@ninjarmm.com>
2020-12-26 16:17:47 -0500

    Added Card and CardList Components

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// Styles
+import './card.styles.css'
+
+export const Card = props => (
+  <div className='card-container'>
+    <img src={ `https://robohash.org/${ props.monster.id }?set=set3` } alt={ `monster-${ props.monster.id }` }/>
+    <h2>{ props.monster.name }</h2>
+    <p>{ props.monster.email }</p>
+  </div>
+)
