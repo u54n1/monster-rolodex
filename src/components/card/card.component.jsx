bad5f3e6dd6dd4f124eacbcc70cd7da3fa06f393
Daniel Usani <daniel.usani@ninjarmm.com>
2020-12-26 16:51:47 -0500

    Added ability to search monsters

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,9 @@ import './card.styles.css'
 
 export const Card = props => (
   <div className='card-container'>
-    <img src={ `https://robohash.org/${ props.monster.id }?set=set3` } alt={ `monster-${ props.monster.id }` }/>
+    <img
+      src={ `https://robohash.org/${ props.monster.id }?set=set3` }
+      alt={ `monster-${ props.monster.id }` }/>
     <h2>{ props.monster.name }</h2>
     <p>{ props.monster.email }</p>
   </div>
